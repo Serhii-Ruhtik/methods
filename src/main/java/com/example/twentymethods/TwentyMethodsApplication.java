34eb9f125e03001d84057424e03a16bb626fd053
Serhii-Ruhtik <fopgavrilyk1@gmail.com>
2024-03-27 19:12:26 +0200

    Методи є питання по 2 і 16

--- src/main/java/com/example/twentymethods/TwentyMethodsApplication.java
+++ src/main/java/com/example/twentymethods/TwentyMethodsApplication.java
@@ -9,7 +9,7 @@ import java.util.Arrays;
 public class TwentyMethodsApplication {
 
     public static void main(String[] args) {
-        //1
+        //1 робила Анастасія
         char[] testArray = {'A', 'B', 'C'};
         charArray(testArray);
 

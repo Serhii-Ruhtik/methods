5c571d322e16506a118259d3a61e9a6ac110021a
Serhii-Ruhtik <fopgavrilyk1@gmail.com>
2024-03-27 17:32:10 +0200

    Методи є питання по 2 і 16

---
+++ src/test/java/com/example/twentymethods/TwentyMethodsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.twentymethods;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwentyMethodsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
